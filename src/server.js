76c5381cc26a4fbbfca84661e2f68b436ce0ea8b
Mikko <kaura1puuro@protonmail.com>
2022-06-28 09:52:24 +0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
+const cors = require('cors')
 const port = process.env.PORT || 5000
 
 connectDB()
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
